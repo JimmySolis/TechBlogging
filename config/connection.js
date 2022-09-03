d26b691dae5bee397260144f9b9ed164e4cf2ed1
Jimmy Solis <jimmysolis14@gmail.com>
2022-09-02 20:06:30 -0400

    Here I have added the connection file in config folder.

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+//   Here I set up my sequelize to allow me to transfer my data.
+//   dotenv will hold my secrets and keep them private.
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+
+//   This will establish that all the needed properites to establish a connection.
+const sequelize = new Sequelize(process.env.MYSQLURI);
+
+
+module.exports = sequelize;
